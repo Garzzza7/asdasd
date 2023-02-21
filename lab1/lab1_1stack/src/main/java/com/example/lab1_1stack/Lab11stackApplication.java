77b2a4a34611b723f0b364dec53c1290d75f265c
Piotr Czapla <234751@edu.p.lodz.pl>
2023-02-21 17:40:11 +0000

    guov

---
+++ lab1/lab1_1stack/src/main/java/com/example/lab1_1stack/Lab11stackApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab1_1stack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab11stackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab11stackApplication.class, args);
+    }
+
+}
---
+++ lab1/lab1_1stack/src/test/java/com/example/lab1_1stack/Lab11stackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab1_1stack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab11stackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
